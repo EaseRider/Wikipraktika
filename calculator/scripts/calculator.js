2ad1c564f317d0b41501d673c4d7d7ba12707f38
= <=>
2016-05-05 01:43:46 +0200

    Wrap KeyPress Listeners inside Closure

--- calculator/scripts/calculator.js
+++ calculator/scripts/calculator.js
@@ -252,85 +252,29 @@ window.addEventListener('load', function() {
 
 	/* Keys */
 
-	document.getElementById("key-0").addEventListener("click", function() {
-		if(document.getElementById("input").innerHTML > 0){
-			document.getElementById("input").innerHTML =  document.getElementById("input").innerHTML + 0;
-		}else{
-			document.getElementById("input").innerHTML = 0;
-		}
 
-	});
+	for (var id = 0; id <= 9; id++) {
+	 (function(id){
+	 	if(id == 0){ 
+		 	document.getElementById("key-0").addEventListener("click", function() {
+				if(document.getElementById("input").innerHTML > 0){
+					document.getElementById("input").innerHTML =  document.getElementById("input").innerHTML + 0;
+				}else{
+					document.getElementById("input").innerHTML = 0;
+				}
+			});
+	 	}else{
+	 		document.getElementById("key-" + id).addEventListener("click", function() {
+				if(document.getElementById("input").innerHTML == 0){
+					document.getElementById("input").innerHTML = id;
+				}else{
+					document.getElementById("input").innerHTML = document.getElementById("input").innerHTML + id;
+				}
+	 		});
+	 	}
+	 	
+	})(id)
 
-	document.getElementById("key-1").addEventListener("click", function() {
-		if(document.getElementById("input").innerHTML == 0){
-			document.getElementById("input").innerHTML = 1;
-		}else{
-			document.getElementById("input").innerHTML = document.getElementById("input").innerHTML + 1;
-		}
-	});
-
-	document.getElementById("key-2").addEventListener("click", function() {
-		if(document.getElementById("input").innerHTML == 0){
-			document.getElementById("input").innerHTML = 2;
-		}else{
-			document.getElementById("input").innerHTML = document.getElementById("input").innerHTML + 2;
-		}
-	});
-
-	document.getElementById("key-3").addEventListener("click", function() {
-		if(document.getElementById("input").innerHTML == 0){
-			document.getElementById("input").innerHTML = 3;
-		}else{
-			document.getElementById("input").innerHTML = document.getElementById("input").innerHTML + 3;
-		}
-	});
-
-	document.getElementById("key-4").addEventListener("click", function() {
-		if(document.getElementById("input").innerHTML == 0){
-			document.getElementById("input").innerHTML = 4;
-		}else{
-			document.getElementById("input").innerHTML = document.getElementById("input").innerHTML + 4;
-		}
-	});
-
-	document.getElementById("key-5").addEventListener("click", function() {
-		if(document.getElementById("input").innerHTML == 0){
-			document.getElementById("input").innerHTML = 5;
-		}else{
-			document.getElementById("input").innerHTML = document.getElementById("input").innerHTML + 5;
-		}
-	});
-
-	document.getElementById("key-6").addEventListener("click", function() {
-		if(document.getElementById("input").innerHTML == 0){
-			document.getElementById("input").innerHTML = 6;
-		}else{
-			document.getElementById("input").innerHTML = document.getElementById("input").innerHTML + 6;
-		}
-	});
-
-	document.getElementById("key-7").addEventListener("click", function() {
-		if(document.getElementById("input").innerHTML == 0){
-			document.getElementById("input").innerHTML = 7;
-		}else{
-			document.getElementById("input").innerHTML = document.getElementById("input").innerHTML + 7;
-		}
-	});
-
-	document.getElementById("key-8").addEventListener("click", function() {
-		if(document.getElementById("input").innerHTML == 0){
-			document.getElementById("input").innerHTML = 8;
-		}else{
-			document.getElementById("input").innerHTML = document.getElementById("input").innerHTML + 8;
-		}
-	});
-
-	document.getElementById("key-9").addEventListener("click", function() {
-		if(document.getElementById("input").innerHTML == 0){
-			document.getElementById("input").innerHTML = 9;
-		}else{
-			document.getElementById("input").innerHTML = document.getElementById("input").innerHTML + 9;
-		}
-	});
+	}
 
 });
